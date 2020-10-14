9b62ace952357f6ebb8dd5b8c70a6cc5f5355b8f
Francisco José <francisco.jsantos@souunit.com.br>
2020-10-14 19:31:37 -0300

    Método getAdjacentes
    implementacao do metodo getAdjacentes

---
+++ TG_2020_2/src/main/java/com/mycompany/tg_2020_2/MetodosBasicos/Adjacentes.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tg_2020_2.MetodosBasicos;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author franc
+ */
+public class Adjacentes {
+
+    static private String caminho = "C:\\git\\tg-2020.2\\TG_2020_2\\file1.txt";
+
+    public static void main(String... args) throws FileNotFoundException {
+
+        int numeroVertices = pegarNumeroVertices();
+
+        List<Integer>[] grafo = new ArrayList[numeroVertices];
+        carregarGrafoEmMemoria(grafo);
+        System.out.println("Adjacentes:");
+        getAdjacentes(grafo);
+    }
+
+    static int pegarNumeroVertices() throws FileNotFoundException {
+        int contador = 0;
+        Scanner input = new Scanner(new FileReader(caminho));
+        while (input.hasNext()) {
+            contador++;
+            input.nextLine();
+        }
+        return contador;
+    }
+
+    static void ligarGrafo(List[] grafo) {
+        for (int i = 0; i < grafo.length; i++) {
+            grafo[i] = new ArrayList<Integer>();
+        }
+    }
+
+    static void carregarGrafoEmMemoria(List[] grafo) throws FileNotFoundException {
+        Scanner input = new Scanner(new FileReader(caminho));
+        ligarGrafo(grafo);
+        int contador = 0;
+        while (input.hasNext()) {
+            String line = input.nextLine();
+            String[] vertices = line.split("\t");
+            for (String vertice : vertices) {
+                grafo[contador].add(vertice);
+            }
+            contador++;
+        }
+    }
+
+    static void getAdjacentes(List[] grafo) {
+        for (int i = 0; i < grafo.length; i++) {
+            System.out.print(grafo[i].get(0) + ":");
+            for (int j = 1; j < grafo[i].size(); j++) {
+                System.out.print(grafo[i].get(j) + " ");
+            }
+
+            System.out.println("");
+
+        }
+
+    }
+
+}
